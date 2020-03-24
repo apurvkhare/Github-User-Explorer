23f878145b96915ac6ef75d05180fc2b3aa4b192
Apurv <apurvkhare1@gmail.com>
2020-03-24 15:07:00 +0530

    Refactored with Context API and deployed to Netlify

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='leading'>
+        The page that you are looking for does not exist
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
